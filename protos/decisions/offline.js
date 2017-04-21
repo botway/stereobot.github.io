10566d496db93fa7bca483796f40681784fe0dac
stereobot <zikorij@yahoo.de>
2017-04-21 21:27:10 +0200

    decisions

---
+++ protos/decisions/offline.js
@@ -0,0 +1,1 @@
+{"version":1492802733269,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","icons/icon-16.png","icons/icon-64.png","icons/loading-logo.png","icons/icon-128.png","icons/icon-256.png","icons/icon-512.png","icons/icon-32.png","register-sw.js"]}
