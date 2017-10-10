8f9f7d7f05b8523115c38a32e4ea35d159b37cba
stereobot <zikorij@yahoo.de>
2017-10-10 12:20:29 +0200

    updd helper

--- protos/helper/offline.js
+++ protos/helper/offline.js
@@ -1,1 +1,1 @@
-{"version":1504951790358,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","icons/loading-logo.png","icons/icon-16.png","icons/icon-32.png","icons/icon-64.png","icons/icon-128.png","icons/icon-256.png","icons/icon-512.png","register-sw.js"]}
+{"version":1507630729437,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","icons/icon-16.png","icons/icon-32.png","icons/icon-64.png","icons/loading-logo.png","icons/icon-128.png","icons/icon-256.png","icons/icon-512.png","register-sw.js"]}
