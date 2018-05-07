9d953cd7cfa66ea41b0d80d60098cb67fb10a437
andrew <zikorij@yahoo.de>
2018-05-07 12:21:11 +0200

    updated

--- protos/helper/offline.js
+++ protos/helper/offline.js
@@ -1,1 +1,1 @@
-{"version":1525541170610,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","icons/icon-32.png","icons/icon-64.png","icons/icon-16.png","icons/loading-logo.png","icons/icon-128.png","icons/icon-256.png","icons/icon-512.png","register-sw.js"]}
+{"version":1525688412295,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","icons/icon-16.png","icons/icon-32.png","icons/loading-logo.png","icons/icon-64.png","icons/icon-128.png","icons/icon-256.png","icons/icon-512.png","register-sw.js"]}
